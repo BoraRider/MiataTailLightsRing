a763cca3d69812d9df5724a916048bd36c437b7e
Maciej Osak <maciek3092@gmail.com>
2025-01-03 23:09:48 +0100

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <Arduino.h>
+#define LED_PIN 8
+
+unsigned long time_1 = 0;
+unsigned long time_2 = 0;
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  time_1 = millis();
+  digitalWrite(LED_PIN, LOW);
+
+  while(time_2 < time_1 + 100)
+  {
+    time_2 = millis();
+  }
+
+  digitalWrite(LED_PIN, HIGH);
+  time_1 = millis();
+  while(time_2 < time_1 + 100)
+  {
+    time_2 = millis();
+  }  
+}
